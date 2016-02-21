d4cdf1c84d74bba6c3678b7a98918004df7b2297
Yoo-Hello <765711543@qq.com>
2016-02-21 18:39:39 +0800

    加入了图片素材

--- js/index.js
+++ js/index.js
@@ -14,19 +14,31 @@ window.onload=function(){
 	var canvas=document.querySelector('#canvas');
 	var ctx=document.querySelector('#canvas').getContext('2d');
     var e=canvas.offsetWidth;
-    console.log(e)
-	
+    
+    var imgT = document.querySelector('#guandaoT');
+    var imgB = document.querySelector('#guandaoB');
+	var xsn1 = document.querySelector('#xsn1');
+    var start = document.querySelector('#start');
+
+
 
 	var draw=function(){
 		ctx.clearRect(0,0,320,568);
 		var sjs=Math.random()*100+180;
 		//画鸟
+        ctx.save();
+        ctx.fillStyle="rgba(255,255,255,0)";
 		a+=0.05;
 		dird.y += a*a;
 		ctx.fillRect(140,dird.y,dird.w,dird.h);
+        ctx.drawImage(xsn1,140,dird.y,dird.w,dird.h);
+        ctx.stroke();
 
 		//管道
+        ctx.save();
 		var vs;
+        
+        //ctx.fillStyle="rgba(255,255,255,0)"
     	for(var i=0;i<guandaoT.length;i++){
     		var d=guandaoT[i];
     		if(recvsrec(dird,d)){
@@ -38,6 +50,7 @@ window.onload=function(){
     			d.h = sjs;
     		}
     		ctx.fillRect(d.x,d.y,d.w,d.h);
+            ctx.drawImage(imgT,d.x,d.y,d.w,d.h);
     	}
     	for(var i=0;i<guandaoB.length;i++){
     		var d=guandaoB[i];
@@ -52,11 +65,13 @@ window.onload=function(){
     			d.y = sjs+128;
     		}
     		ctx.fillRect(d.x,d.y,d.w,d.h);
+            ctx.drawImage(imgB,d.x,d.y,d.w,d.h);
+
     	}
     	if(vs){
     		return;
     	}
-		
+		ctx.stroke();
 		//边界判断
 		if(dird.y>=568-40||dird.y<= 0){
 			return;
@@ -64,7 +79,14 @@ window.onload=function(){
 			window.requestAnimationFrame(draw);
 		}
 	}
-	window.requestAnimationFrame(draw);
+
+    //开始游戏按钮
+    start.onclick=function(){
+        window.requestAnimationFrame(draw);
+        start.style.display="none";
+    }
+
+    //自由落体
 	canvas.onclick=function(){
 		a=1;
 		dird.y-=40;
@@ -72,15 +94,15 @@ window.onload=function(){
     
     //判定碰撞柱子
 	var recvsrec =  function(rect0,rect1){
-    if (rect0.x >= rect1.x && rect0.x >= rect1.x + rect1.w) {
-      return false;
-    } else if (rect0.x <= rect1.x && rect0.x + rect0.w <= rect1.x) {
-      return false;
-    } else if (rect0.y >= rect1.y && rect0.y >= rect1.y + rect1.h) {
-      return false;
-    } else if (rect0.y <= rect1.y && rect0.y + rect0.h <= rect1.y) {
-      return false;
-    }
+        if (rect0.x >= rect1.x && rect0.x >= rect1.x + rect1.w) {
+            return false;
+        } else if (rect0.x <= rect1.x && rect0.x + rect0.w <= rect1.x) {
+             return false;
+        } else if (rect0.y >= rect1.y && rect0.y >= rect1.y + rect1.h) {
+            return false;
+        } else if (rect0.y <= rect1.y && rect0.y + rect0.h <= rect1.y) {
+            return false;
+        }
     return true;
     };
 }
